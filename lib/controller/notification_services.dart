9ce7139484c8bece05707920fa5507b2474515cf
kishorec11 <kishorekumar@dckap.com>
2024-07-24 16:35:37 +0530

    done

---
+++ lib/controller/notification_services.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class PushNotifications {
+  static final _firebaseMessaging = FirebaseMessaging.instance;
+  static final FlutterLocalNotificationsPlugin
+      _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  // request notification permission
+  static Future init() async {
+    await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+    final token = await _firebaseMessaging.getToken();
+    print("Token : $token");
+  }
+}
